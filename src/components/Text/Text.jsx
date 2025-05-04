66f5ce75979ed7c2736e2f6e43dd7c5bb741d36c
Alaa Karem <alaakarem775@gmail.com>
2025-05-04 17:59:19 +0300

    finaaaaaaaaaaaaaaaal

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Text() {
     return (
         <>
-            <section className="bg-back-ground text-center lg:text-left lg:mb-15  text-white px-6 md:px-20 py-32 font-normal font-azonix">
-                <div className=" space-y-4 text-3xl pl-8 leading-relaxed ">
+            <section className="bg-back-ground text-center lg:text-left lg:mb-15 py-20  text-white px-6 md:px-20 lg:py-32 font-normal font-azonix">
+                <div className=" lg:space-y-4 text-3xl pl-8 leading-relaxed ">
                     <p className='text-2xl lg:text-6xl'>
                         GREAT BUSINESSES{' '}
                     </p>
