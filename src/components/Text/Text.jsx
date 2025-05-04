37a0f0247e1829b7315e8bc7e7271e1365cef607
Alaa Karem <alaakarem775@gmail.com>
2025-05-04 17:53:34 +0300

    final text

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -5,12 +5,12 @@ export default function Text() {
         <>
             <section className="bg-back-ground text-center lg:text-left lg:mb-15  text-white px-6 md:px-20 py-32 font-normal font-azonix">
                 <div className=" space-y-4 text-3xl pl-8 leading-relaxed ">
-                    <p className='text-3xl lg:text-6xl'>
+                    <p className='text-2xl lg:text-6xl'>
                         GREAT BUSINESSES{' '}
                     </p>
-                    <span className="text-transparent text-3xl font-normal bg-clip-text bg-gradient-to-r from-orange-500 to-orange-300 lg:text-6xl">DON'T</span>{' '}
+                    <span className="text-transparent text-2xl font-normal bg-clip-text bg-gradient-to-r from-orange-500 to-orange-300 lg:text-6xl">DON'T</span>{' '}
                     <span className="text-transparent text-3xl font-normal bg-clip-text bg-text-color lg:text-6xl">FAIL</span>
-                    <p className='text-3xl lg:text-6xl'>BAD SOFTWARE DOES.</p>
+                    <p className='text-2xl lg:text-6xl'>BAD SOFTWARE DOES.</p>
                 </div>
             </section>
         </>
