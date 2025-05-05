e7e04cff88b193222b728f86651d8ef041b34f09
Alaa Karem <alaakarem775@gmail.com>
2025-05-05 14:22:48 +0300

    text2 responsive

--- src/components/Text2/Text2.jsx
+++ src/components/Text2/Text2.jsx
@@ -3,26 +3,26 @@ import React from "react";
 export default function Text2() {
   return (
     <>
-      <section className="bg-[#02021E] py-20 px-6 text-white flex flex-row items-center justify-center gap-40">
+      <section className="bg-[#02021E] px-2 lg:py-20 lg:px-6 text-white flex flex-col gap-5 lg:flex-row items-center justify-center lg:gap-40">
         {/* الزر */}
 
-        <div className="inline-block rounded-full bg-gradient-to-r from-[#FF6F00] to-[#007BFF] p-[1px] mb-24">
+        <div className="lg:inline-block rounded-full bg-gradient-to-r from-[#FF6F00] to-[#007BFF] p-[1px] lg:mb-24">
           <div className="flex items-center justify-center rounded-full font-azonix font-normal bg-[#02021E] px-8 py-3">
-            <span className="text-[#FF6F00]  text-xl">+ 30</span>
+            <span className="text-[#FF6F00] text-xl">+ 30</span>
             <span className="text-white text-xl ml-3">REVIEWS</span>
           </div>
         </div>
 
         {/* النص */}
-        <p className="max-w-3xl leading-11  tracking-wide ">
-          <span className="ml-28 text-transparent bg-clip-text bg-gradient-to-r from-[#FF6F00]  to-[#007BFF] font-azonix font-normal text-4xl">
+        <p className="lg:max-w-3xl leading-8 lg:leading-11 tracking-wide text-center">
+          <span className="lg:ml-28 text-lg text-transparent bg-clip-text bg-gradient-to-r from-[#FF6F00]  to-[#007BFF] font-azonix font-normal lg:text-4xl">
             FROM STARTUPS TO GROWING
           </span>
-          <span className=" font-azonix font-normal text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#FF6F00] to-[#bb733b]">
+          <span className=" font-azonix text-lg font-normal lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#FF6F00] to-[#bb733b]">
             {" "}
             BUSINESSES
           </span>
-          <span className="font-azonix text-2xl font-normal text-left">
+          <span className="font-azonix text-lg max-w-xl lg:text-2xl font-normal lg:text-left">
             , WE'VE EMPOWERED OUR CLIENTS TO ACHIEVE THEIR GOALS— AND THEIR
             SUCCESS STORIES ARE OUR GREATEST REWARD
           </span>
