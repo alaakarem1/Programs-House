742975a9514eb3a568af16fc75c96404406e9dad
Alaa Karem <alaakarem775@gmail.com>
2025-08-24 11:51:50 +0300

    feat: add Prettier and Tailwind CSS plugin for code formatting

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,28 +6,28 @@ import vectorImg from "../../assets/Vector 1.png";
 import { motion } from "framer-motion";
 export default function Hero() {
   return (
-    <div className=" lg:relative lg:h-full lg:flex lg:mx-8 bg-back-ground lg:overflow-hidden ">
+    <div className="bg-back-ground lg:relative lg:mx-8 lg:flex lg:h-full lg:overflow-hidden">
       <img
         src={background}
-        className="hidden lg:block lg:absolute lg:h-full lg:w-full lg:top-0 lg:z-0  "
+        className="hidden lg:absolute lg:top-0 lg:z-0 lg:block lg:h-full lg:w-full"
         alt="Background Image"
       />
-      <div className="relative z-10 flex flex-col md:flex lg:flex-row items-center w-full justify-around px-8 pt-7  ">
+      <div className="relative z-10 flex w-full flex-col items-center justify-around px-8 pt-7 md:flex-row md:gap-5">
         {/* النصوص */}
-        <div className=" lg:space-y-6 text-white">
-          <h1 className=" hero-title font-azonix text-4xl lg:text-7xl lg:ml-7">
+        <div className="text-white md:space-y-4 lg:space-y-6">
+          <h1 className="hero-title font-azonix text-4xl lg:ml-7 lg:text-7xl">
             WE BELIEVE
           </h1>
 
-          <p className="text-xl text-center  font-normal leading-relaxed font-azonix lg:text-4xl lg:pt-5 lg:ml-5 lg:text-left">
+          <p className="font-azonix text-center text-xl leading-relaxed font-normal lg:ml-5 lg:pt-5 lg:text-left lg:text-4xl">
             THERE'S NO BAD{" "}
           </p>
-          <p className="text-xl text-center font-normal leading-relaxed font-azonix lg:ml-5 lg:text-4xl">
+          <p className="font-azonix text-center text-xl leading-relaxed font-normal lg:ml-5 lg:text-4xl">
             BUSINESS, THERE'S BAD
           </p>
 
-          <h1 className="text-4xl font-bold text-center lg:text-start">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r  from-[#FF6F00]  to-[#007BFF] font-azonix font-normal text-3xl text-center lg:text-6xl lg:ml-5">
+          <h1 className="text-center text-4xl font-bold lg:text-start">
+            <span className="font-azonix bg-gradient-to-r from-[#FF6F00] to-[#007BFF] bg-clip-text text-center text-3xl font-normal text-transparent lg:ml-5 lg:text-6xl">
               DEVELOPER
             </span>
           </h1>
@@ -41,9 +41,9 @@ export default function Hero() {
                   ease: "circInOut",
                 },
               }}
-              className="relative inline-block ml-7 mt-100 p-[2px] rounded-full bg-[length:200%_200%] bg-gradient-to-r from-dis-btn via-btn-color to-btn-color lg:mt-20 lg:ml-36 lg:mx-14 lg:p-[2px]"
+              className="from-dis-btn via-btn-color to-btn-color relative mt-100 ml-7 inline-block rounded-full bg-gradient-to-r bg-[length:200%_200%] p-[2px] md:mt-0 lg:mx-14 lg:mt-20 lg:ml-36 lg:p-[2px]"
             >
-              <button className="relative z-10 px-4 py-1 w-full h-full rounded-full bg-dis-btn flex items-center justify-center lg:z-10 lg:px-7 lg:py-3 lg:relative lg:flex lg:items-center lg:justify-center">
+              <button className="bg-dis-btn relative z-10 flex h-full w-full items-center justify-center rounded-full px-4 py-1 lg:relative lg:z-10 lg:flex lg:items-center lg:justify-center lg:px-7 lg:py-3">
                 <span className="font-nebula text-sm tracking-widest text-white lg:text-xl">
                   DISCOVER NOW
                 </span>
@@ -57,7 +57,7 @@ export default function Hero() {
                 >
                   <circle cx="20" cy="20" r="20" fill="white" />
                   <foreignObject x="6" y="6" width="28" height="28">
-                    <img src={Rocket} alt="icon" className="w-full h-full" />
+                    <img src={Rocket} alt="icon" className="h-full w-full" />
                   </foreignObject>
                 </svg>
               </button>
@@ -65,27 +65,28 @@ export default function Hero() {
           </a>
         </div>
 
-        {/* الروبوت */}
-        <div className="z-10 lg:relative lg:mr-20 lg:z-10 ">
-          <img
-            src={robotImg}
-            alt="robot"
-            className="absolute overflow-hidden drop-shadow-[0_0_8px_#5f4fd8] top-50 right-20 w-[250px] lg:w-[550px] lg:relative lg:top-0 lg:left-10 md:left-66  lg:overflow-hidden"
-          />
-        </div>
-
-        {/* النص الثابت */}
-        <div className="text-xl absolute right-13 top-[40%] font-glitch font-bold text-white leading-tight z-1 pointer-events-none md:right-55 lg:absolute lg:right-25 lg:top-[20%] lg:text-left lg:text-5xl lg:font-glitch lg:font-bold  lg:text-white lg:leading-tight ">
-          <p>FOR</p>
-          <p>EVERY</p>
-          <p>BUSINESS</p>
+        <div>
+          {/* الروبوت */}
+          <div className="z-10 md:relative lg:z-10 lg:mr-20">
+            <img
+              src={robotImg}
+              alt="robot"
+              className="absolute top-50 right-20 md:top-auto md:right-auto w-[250px] overflow-hidden drop-shadow-[0_0_8px_#5f4fd8] md:relative lg:top-0 lg:left-10 lg:w-[550px] lg:overflow-hidden"
+            />
+          </div>
+          {/* النص الثابت */}
+          <div className="font-glitch lg:font-glitch pointer-events-none absolute top-[40%] right-13 z-1 text-xl leading-tight font-bold text-white md:top-[25%] md:right-12 lg:absolute lg:top-[20%] lg:right-25 lg:text-left lg:text-5xl lg:leading-tight lg:font-bold lg:text-white">
+            <p>FOR</p>
+            <p>EVERY</p>
+            <p>BUSINESS</p>
+          </div>
         </div>
       </div>
       <img
         src={vectorImg}
         alt=""
         style={{ justifySelf: "anchor-center" }}
-        className="hidden lg:absolute lg:bottom-0 lg:w-96 lg:z-60 lg:right-0.5 lg:block"
+        className="hidden lg:absolute lg:right-0.5 lg:bottom-0 lg:z-60 lg:block lg:w-96"
       />
     </div>
   );
